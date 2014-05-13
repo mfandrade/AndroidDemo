47f15e51b91d309f16db0943659d3319cfc1047e
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-13 09:32:27 -0300

    Listagem básica de alunos

---
+++ src/com/wordpress/mfandrade/cadastro/activity/ListaAlunosActivity.java
@@ -0,0 +1,20 @@
+package com.wordpress.mfandrade.cadastro.activity;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import com.wordpress.mfandrade.cadastro.*;
+
+public class ListaAlunosActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_alunos);
+		//
+		Aluno[] alunos = { new Aluno("Huguinho"), new Aluno("Zezinho"), new Aluno("Luizinho") };
+		ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+		ListView lstAlunos = (ListView) findViewById(R.id.list_alunos_lstAlunos);
+		lstAlunos.setAdapter(adapter);
+	}
+}
