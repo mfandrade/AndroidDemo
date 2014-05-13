1d3f84c6299714a58f028e4d30350d7d58dcac55
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-13 09:56:26 -0300

    Activity form

---
+++ src/com/wordpress/mfandrade/cadastro/FormAlunoActivity.java
@@ -0,0 +1,13 @@
+package com.wordpress.mfandrade.cadastro;
+
+import android.app.*;
+import android.os.*;
+
+public class FormAlunoActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_form_aluno);
+	}
+}
