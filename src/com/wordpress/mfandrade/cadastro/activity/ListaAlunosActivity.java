0c93d0c950492785f809e739c10bb6786b7d73d8
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-13 15:56:54 -0300

    Carrega adequadamente os dados na lista

--- src/com/wordpress/mfandrade/cadastro/activity/ListaAlunosActivity.java
+++ src/com/wordpress/mfandrade/cadastro/activity/ListaAlunosActivity.java
@@ -12,35 +12,42 @@ import com.wordpress.mfandrade.cadastro.*;
 import com.wordpress.mfandrade.cadastro.dao.*;
 
 public class ListaAlunosActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-	
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_lista_alunos);
-		//
-		AlunoDAO dao = new AlunoDAO(this);
-		List<Aluno> alunos = dao.getAll();
-		ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
-		ListView lstAlunos = (ListView) findViewById(R.id.list_alunos_lstAlunos);
-		lstAlunos.setAdapter(adapter);
+    private ListView _lstAlunos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_lista_alunos);
+	_lstAlunos = (ListView) findViewById(R.id.list_alunos_lstAlunos);
+    }
+
+    @Override
+    protected void onResume() {
+	super.onResume();
+	//
+	AlunoDAO dao = new AlunoDAO(this);
+	List<Aluno> alunos = dao.getAll();
+	ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
+	_lstAlunos.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+	switch (item.getItemId()) {
+	case R.id.lista_alunos_menu_novo:
+	    Intent formAluno = new Intent(this, FormAlunoActivity.class);
+	    startActivity(formAluno);
+	    return false;
 	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-	
-		switch (item.getItemId()) {
-		case R.id.lista_alunos_menu_novo:
-			Intent formAluno = new Intent(this, FormAlunoActivity.class);
-			startActivity(formAluno);
-			return false;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-	
-		getMenuInflater().inflate(R.menu.lista_alunos_menu, menu);
-		return true;
-	}
+	return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+	getMenuInflater().inflate(R.menu.lista_alunos_menu, menu);
+	return true;
+    }
 }
