e3473dc4221f1c9f38a4655c028490aa8301f87a
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-13 13:03:23 -0300

    Inclui (options) menu

--- src/com/wordpress/mfandrade/cadastro/activity/ListaAlunosActivity.java
+++ src/com/wordpress/mfandrade/cadastro/activity/ListaAlunosActivity.java
@@ -2,6 +2,7 @@ package com.wordpress.mfandrade.cadastro.activity;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
 import android.widget.*;
 import com.wordpress.mfandrade.cadastro.*;
 
@@ -16,5 +17,12 @@ public class ListaAlunosActivity extends Activity {
 		ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
 		ListView lstAlunos = (ListView) findViewById(R.id.list_alunos_lstAlunos);
 		lstAlunos.setAdapter(adapter);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	
+		getMenuInflater().inflate(R.menu.lista_alunos_menu, menu);
+		return true;
 	}
 }
