d8c19ecd01c645593fd7f940f51b22faabeefc30
Marcelo F Andrade <mfandrade@gmail.com>
2014-05-13 08:57:56 -0300

    Projeto criado

---
+++ src/com/wordpress/mfandrade/cadastro/ListaAlunosActivity.java
@@ -0,0 +1,13 @@
+package com.wordpress.mfandrade.cadastro;
+
+import android.app.*;
+import android.os.*;
+
+public class ListaAlunosActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lista_alunos);
+	}
+}
